6e52c2584d1f2b279b8b0e8a0f51c49f7616a9b2
DESKTOP-LICL3E7\user <bough157@gmail.com>
2025-12-12 05:31:15 +0900

    포스트 작성에서 Liquid 코드 작성하기 1차 완료

---
+++ raft/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1765480363',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/about/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
